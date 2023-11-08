119d5e1f07d2505b9872dc5b208cf0243081f10e
Elias Moura <elias.smourayt@gmail.com>
2023-11-08 15:54:30 -0300

    Ajustando footer.

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -6,13 +6,10 @@ function Footer () {
             <div className="container flex flex-col items-center py-4">
             <h2 className="text-xl font-bold">Mais Saúde App</h2>
             <ul className="list-none">
-                <li className="flex items-center">
-                    <GithubLogo size={48} weight="bold"/> 
-                    <a href="https://github.com/MaisSaudeGen">Repositório do Projeto</a>
-                </li>
+                <li className="flex justify-center items-center">
+                    <a className="flex flex-col justify-center items-center" href="https://github.com/MaisSaudeGen"><GithubLogo size={48} weight="bold"/><span>Repositório do Projeto</span></a>                </li>
             </ul>
             </div>
-
         </footer>
     )
 }
