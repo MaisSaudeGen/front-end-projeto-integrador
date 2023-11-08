e2be388d45ffca37fe892c55f9ee8c1174e7b513
Elias Moura <elias.smourayt@gmail.com>
2023-11-08 15:52:48 -0300

    sobre page

---
+++ frontend/src/pages/sobre/sobre.tsx
@@ -0,0 +1,21 @@
+import sobreImg from '../../../public/images/sobre/building.jpg';
+
+export default function Sobre() {
+  return (
+    <main className="flex grow bg-cyan-800">
+    <div className="flex justify-center items-center py-8 px-10">
+      <div className="w-1/3 mr-4 flex flex-col items-center ">
+          <div className="pb-6">
+               <h1 className="text-3xl font-bold text-white  text-center">Página em construção</h1>
+          </div>
+       
+        <p className="text-xl text-white">
+          Em breve você terá acesso as informações de sobre do nosso projeto.
+        </p>
+      </div>
+
+      <img className="w-1/3 rounded-md" src={sobreImg}></img>
+    </div>
+  </main>
+  )
+}
