fefc3d5f6e8eff241dc027537c2af55a4e729bb6
Elias Moura <elias.smourayt@gmail.com>
2023-11-08 15:54:05 -0300

    cadastrando rotas.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,26 @@ import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Cadastrar from "./pages/cadastro/Cadastrar";
-
-
+import Login from "./pages/login/Login";
+import Sobre from "./pages/sobre/sobre";
+import NotFound from "./pages/not-found/NotFound";
 
 function App() {
   return (
-      <div className="min-h-screen flex flex-col bg-indigo-600">
-        <BrowserRouter>
-        <Navbar/>
-            <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/Cadastrar" element={<Cadastrar/>}/>   
-             </Routes>
-          <Footer/>
-        </BrowserRouter>
-      </div>
-  )
+    <div className="min-h-screen flex flex-col bg-indigo-600">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastrar" element={<Cadastrar />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
