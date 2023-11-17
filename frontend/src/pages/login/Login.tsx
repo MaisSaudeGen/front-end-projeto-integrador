6908cb15ceb01b66adea0402ae64cfe85eb4aeb1
Elias Moura <elias.smourayt@gmail.com>
2023-11-17 19:35:07 -0300

    Login mais limpo

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,101 +1,144 @@
-import loginImg from '../../../public/images/login/login.jpg';
-import React, { ChangeEvent, FormEvent, useContext, useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../contexts/AuthContext';
-import UsuarioLogin from '../../model/UsuarioLogin';
-import { RotatingLines } from 'react-loader-spinner';
-import Cadastrar from '../cadastro/Cadastrar';
+import loginImg from "../../assets/images/login/login.png";
+import { ChangeEvent, useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import toastAlert from "../../utils/toastAlert";
 
-
-export default function Login(){
-
-  let navigate = useNavigate();
-
-  // const [usuarioLogin, setUsuarioLogin] = useState<UsuarioLogin>(
-  //   {} as UsuarioLogin
-  // );
-
-  const [usuarioLogin, setUsuarioLogin] = useState({usuario:"", senha:""})
+export default function Login() {
+  const [usuarioLogin, setUsuarioLogin] = useState({ usuario: "", senha: "" });
   const { usuario, handleLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const {isLoading} = useContext(AuthContext) 
-
-  useEffect(() => {
-    if (usuario.token !== "") {
-        navigate('/sobre')
-    }
-}, [usuario.token])
-
-function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  setUsuarioLogin({
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuarioLogin({
       ...usuarioLogin,
-      [e.target.name]: e.target.value
-  })
-}
+      [e.target.name]: e.target.value,
+    });
+  }
 
-function login(e: ChangeEvent<HTMLFormElement>) {
+  async function login(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-  e.preventDefault()
+    toastAlert("Carregando", "info", 500)
+    const resultado = await handleLogin(usuarioLogin);
 
-  console.log (usuarioLogin)
-
-  handleLogin(usuarioLogin)
-}
-
+    resultado.includes('403') || resultado.includes('401') && (
+      toastAlert("Email ou senha incorretos.", "error")
+    )
+    
+    if(resultado.includes('200')){
+      toastAlert("Usuário logado.", "success");
+      navigate('/postagens')
+    }
+  }
 
   return (
-    <div className="flex grow justify-center items-center bg-green-500">
-      <div className="flex justify-center flex-row-reverse">
-        <form
-          onSubmit={login}
-          className="flex 
-          flex-col  justify-center items-center bg-blue-900 p-10 rounded-lg drop-shadow-lg"
+    <div
+      className="flex grow justify-center items-center
+    bg-gradient-to-r from-secundario-3 to-principal-3
+    "
+    >
+      <div
+        className="
+            rounded-xl mx-8 px-8 
+            flex justify-center text-white
+            shadow-2xl bg-black bg-opacity-40
+            
+        "
+      >
+        <div
+          className="
+          flex flex-col items-center justify-center my-10 
+          "
         >
-          <h2 className="text-2xl font-bold text-bold text-white pb-6">
-            Entrar
-          </h2>
+          <img className="w-[250px] mb-4" src={loginImg} alt="Logo mais saúde." />
+          <form onSubmit={login}>
+            <div className="flex text-lg flex-col w-full mb-2">
+              <label className="text-white text-lg" htmlFor="usuario">
+                Email
+              </label>
+              <input
+                required
+                type="email"
+                id="usuario"
+                name="usuario"
+                placeholder="voce@exemplo.com"
+                className="
+                rounded-md p-1 text-lg text-black
+                focus:scale-[1.02]
+                focus:shadow-2xl
+                focus:outline-none"
+                value={usuarioLogin.usuario}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              ></input>
+              {
+              usuarioLogin.usuario.length === 0 || 
+                ( 
+                  usuarioLogin.usuario.includes(".") &&
+                  usuarioLogin.usuario.includes("@")
+                ) ||
+              <span className="text-red-500 text-center pt-1">
+                Email deve incluir '.' e '@'
+              </span>
+              }
+            </div>
 
-          <div className="flex flex-col  mb-2">
-            <label className="text-white text-lg" htmlFor="usuario">
-              Email
-            </label>
-            <input
-              type="usuario"
-              id="usuario"
-              name="usuario"
-              placeholder="email"
-              className="rounded-md p-1"
-              value={usuarioLogin.usuario} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            ></input>
-          </div>
+            <div className="flex flex-col">
+              <label className="text-white bold text-lg" htmlFor="senha">
+                Senha
+              </label>
+              <input
+                required
+                type="password"
+                id="senha"
+                name="senha"
+                placeholder="Digite 8 caracteres ou mais"
+                className="
+                rounded-md p-1 text-lg text-black
+                focus:scale-[1.02]
+                focus:shadow-2xl
+                focus:outline-none"
+                value={usuarioLogin.senha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  atualizarEstado(e)
+                }
+              ></input>
+              { (usuarioLogin.senha.length > 0 && usuarioLogin.senha.length < 8 ) &&
+              <span className="text-red-500 text-center pt-1">
+                Digite no mínimo 8 caracteres.
+              </span>
+              }
+            </div>
 
-          <div className="flex flex-col">
-            <label className="text-white text-lg" htmlFor="senha">
-              Senha
-            </label>
-            <input
-              type="password"
-              id="senha"
-              name="senha"
-              placeholder="Senha"
-              className="rounded-md p-1"
-              value={usuarioLogin.senha} 
-              onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-            ></input>
-          </div>
+            <div className="flex flex-col justify-center">
+              <button
+                type="submit"
 
-          <div>
-           
-            <button type='submit' className="mx-3 mt-6 text-white bg-green-700 p-3 rounded-md hoover:bg-orange-500" >Entrar</button >
-            <button onClick={()=>{navigate("/cadastrar")}} className="mx-3 mt-6 text-white p-3 rounded-md hoover:bg-orange-500 border">Cadastre-se
-            </button>
-          </div>
-        </form>
-        <div className="max-w-[50%] ">
-          <img className="rounded-md" src={loginImg} alt="" />
+                className=" mt-4
+                text-md
+                self-center
+                text-white bg-green-700 
+                py-2 px-6 rounded-md hover:bg-green-600
+                font-medium	
+                "
+              >
+                Entrar
+              </button>
+              <span className="text-white p-3">
+                Ainda não possue uma conta?
+                <Link
+                  to={"/cadastrar"}
+                  className="underline pl-1 text-blue-400 "
+                >
+                  Cadastre-se
+                </Link>
+              </span>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
