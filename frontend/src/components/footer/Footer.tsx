8ad47aa8bb048a9c41f8484dccc8ca3a25727c7b
Elias Moura <elias.smourayt@gmail.com>
2023-11-17 19:33:35 -0300

    Footer e navbar somente se logado

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,17 +1,32 @@
 import { GithubLogo } from "@phosphor-icons/react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
-function Footer () {
-    return (
-        <footer className="flex justify-center bg-indigo-900 text-white">
-            <div className="container flex flex-col items-center py-4">
-            <h2 className="text-xl font-bold">Mais Saúde App</h2>
-            <ul className="list-none">
-                <li className="flex justify-center items-center">
-                    <a className="flex flex-col justify-center items-center" href="https://github.com/MaisSaudeGen"><GithubLogo size={48} weight="bold"/><span>Repositório do Projeto</span></a>                </li>
-            </ul>
-            </div>
-        </footer>
-    )
+function Footer() {
+
+    const { usuario } = useContext(AuthContext);
+    const exibir = usuario.token || "hidden" 
+
+  return (
+    <footer className=
+    {`flex justify-center bg-indigo-900 text-white ${exibir}`}
+    >
+      <div className="container flex flex-col items-center py-4">
+        <h2 className="text-xl font-bold">Mais Saúde App</h2>
+        <ul className="list-none">
+          <li className="flex justify-center items-center">
+            <a
+              className="flex flex-col justify-center items-center"
+              href="https://github.com/MaisSaudeGen"
+            >
+              <GithubLogo size={48} weight="bold" />
+              <span>Repositório do Projeto</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
