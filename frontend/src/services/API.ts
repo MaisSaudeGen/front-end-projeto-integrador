a1d342f39cf6789d8959f784d68f477e7cc03fe5
Elias Moura <elias.smourayt@gmail.com>
2023-11-18 17:50:15 -0300

    nova config da api

--- frontend/src/services/API.ts
+++ frontend/src/services/API.ts
@@ -1,6 +1,22 @@
 import axios from "axios";
+import { getUserLocalStorage } from "../contexts/authProvider/Utils";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: 'https://mais-saude-app.onrender.com'
 })
 
+api.interceptors.request.use(
+    (config) => {
+      const user = getUserLocalStorage()
+  
+      config.headers.Authorization = user?.token;
+  
+      return config
+    },
+    (error) => {
+      //enviar para login
+      return Promise.reject(error)
+    },
+  )
+  
+export default api;
