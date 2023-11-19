8cdddde877bbf1a53b992fd6d96c02ffaf8b6e28
Elias Moura <elias.smourayt@gmail.com>
2023-11-19 14:21:55 -0300

    retirando import desnecessário.

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { Children, ReactNode } from "react"
+import { ReactNode } from "react"
 
 interface Props {
   isOpen: boolean
@@ -13,11 +12,10 @@ export default function Modal({isOpen, setOpen, descricao, titulo, children}: Pr
   if (isOpen){
     return (
       <div className="bg-blue-900 fixed top-0 right-0 left-0 bottom-0 bg-opacity-70 ">
-        <div className=
-        "fixed left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%]
-        bg-blue-900 p-8 rounded-xl
-        flex flex-col items-center shadow-2xl border
-        ">
+        <div className="fixed left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%]
+          bg-blue-900 p-8 rounded-xl
+          flex flex-col items-center shadow-2xl border"
+        >
           <h2 className="text-xl font-bold text-white pb-4">{titulo}</h2>
           <p className="">{descricao}</p>
           {children}
