3d6c78a721162b30494d05bb6259bb4b10cd41f6
Elias Moura <elias.smourayt@gmail.com>
2023-11-19 14:51:00 -0300

    Subindo a posição dos botões e barra de pesquisa

--- frontend/src/pages/categorias/PageCategorias.tsx
+++ frontend/src/pages/categorias/PageCategorias.tsx
@@ -2,12 +2,24 @@ import { useEffect, useState } from "react";
 import ListaCategorias from "../../components/categorias/listaCategorias/ListaCategorias";
 import Modal from "../../components/Modal";
 import FormCriarCategoria from "../../components/categorias/FormCriarCategoria";
+import BarraPesquisa from "../../components/BarraPesquisa/BarraPesquisa";
 
 export default function PageCategorias() {
   const [open, setOpen] = useState(false)
 
   return (
-    <div className="flex flex-col items-center justify-around grow">
+    <div className="flex flex-col items-center justify-center grow my-4">
+        <div className="flex gap-4 items-center">
+          <button
+            onClick={() => {setOpen(!open)}}
+            className=" text-white border p-4 rounded-md shadow-md
+              bg-black bg-opacity-40
+            hover:bg-blue-400"
+            >
+            Criar nova categoria
+          </button>
+         <BarraPesquisa/>
+        </div>
       <div className="flex justify-center w-full">
         <Modal isOpen={open} setOpen={setOpen}
         titulo="Criar Categoria"
@@ -17,12 +29,6 @@ export default function PageCategorias() {
         </Modal>
         <ListaCategorias />
       </div>
-        <div className="my-4">
-          <button
-          onClick={() => {setOpen(!open)}}
-          className="border p-4 rounded-md shadow-md hover:bg-blue-300"
-          >Criar nova categoria</button>
-        </div>
     </div>
   );
 }
