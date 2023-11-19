6529d3171fbc72c0c51b90ab32aedafcf1851b4c
Elias Moura <elias.smourayt@gmail.com>
2023-11-19 20:46:46 -0300

    formatação

--- frontend/src/components/ConfirmModal/ConfirmModal.tsx
+++ frontend/src/components/ConfirmModal/ConfirmModal.tsx
@@ -20,8 +20,18 @@ export const ConfirmModal = ({ isOpen, mostrarModal, onConfirm, mensagem, titulo
         <h2 className="text-2xl font-bold text-white pb-4" >{mensagem}</h2>
         <p className="text-xl font-bold text-white pb-4">{tituloCard}</p>
         <div className="flex gap-4">
-        <button className="py-2 px-8 border text-white rounded-md bg-green-600 hover:bg-green-400" onClick={() => {onConfirm(); mostrarModal(false)}}>Sim</button>
-        <button className="py-2 px-8 border text-white rounded-md bg-red-600 hover:bg-red-400" onClick={() => {mostrarModal(false)}}>Não</button>
+        <button className="py-2 px-8 border text-white rounded-md bg-green-600 hover:bg-green-400" 
+          onClick={
+          () => {onConfirm(); mostrarModal(false)}
+          }>
+            Sim
+        </button>
+        <button className="py-2 px-8 border text-white rounded-md bg-red-600 hover:bg-red-400" 
+          onClick={
+          () => {mostrarModal(false)}
+          }>
+            Não
+        </button>
         </div>
       </div>
     </div>
