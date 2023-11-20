a98c924432b11131d348e43994660817f25c6c19
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 10:26:48 -0300

    fromatação

--- frontend/src/pages/categorias/PageCategorias.tsx
+++ frontend/src/pages/categorias/PageCategorias.tsx
@@ -14,13 +14,9 @@ export default function PageCategorias() {
     <CategoriaPesquisadaProvider>
       <section className="flex py-6 gap-4 items-center justify-center bg-black bg-opacity-30">
         <button
-          onClick={() => {
-            setOpen(!open);
-          }}
-          className=" text-white border p-4 rounded-md shadow-md
-              bg-black bg-opacity-40
-            hover:bg-blue-400"
-        >
+          onClick={() => setOpen(!open)}
+          className="text-white border p-4 rounded-md shadow-md bg-black bg-opacity-40 hover:bg-blue-400"
+         >
           Criar nova categoria
         </button>
         <BarraPesquisa />
@@ -32,7 +28,7 @@ export default function PageCategorias() {
             isOpen={open}
             titulo="Criar Categoria"
             descricao="Preencha os campos abaixo para criar uma nova categoria."
-          >
+            >
             <FormCriarCategoria setOpen={setOpen} />
           </Modal>
           <ListaCategorias />
