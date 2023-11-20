b91974e0f8b50b404bf96602e664add119cd145a
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 01:21:51 -0300

    Ajustando layout

--- frontend/src/components/Modal/index.tsx
+++ frontend/src/components/Modal/index.tsx
@@ -2,26 +2,23 @@ import { ReactNode } from "react"
 
 interface Props {
   isOpen: boolean
-  setOpen: (isOpen: boolean) => void
   descricao: string
   titulo: string
   children: ReactNode
 }
 
-export default function Modal({isOpen, setOpen, descricao, titulo, children}: Props) {
+export default function Modal({isOpen, descricao, titulo, children}: Props) {
   if (isOpen){
     return (
       <div className="bg-blue-900 fixed top-0 right-0 left-0 bottom-0 bg-opacity-70 ">
         <div className="fixed left-[50%] top-[50%] translate-y-[-50%] translate-x-[-50%]
-          bg-blue-900 p-8 rounded-xl
+          bg-zinc-800 p-8 rounded-xl gap-2
           flex flex-col items-center shadow-2xl border"
-        >
-          <h2 className="text-xl font-bold text-white pb-4">{titulo}</h2>
-          <p className="">{descricao}</p>
+          >
+          <h2 className="text-2xl font-bold text-white pb-2">{titulo}</h2>
+          <p className="text-white w-[90%]">{descricao}</p>
           {children}
-          <button 
-          className="border py-2 px-8 bg-red-400 text-white rounded-md shadow-md hover:bg-blue-300"
-          onClick={() => setOpen(!isOpen) }>Fechar</button>
+
         </div>
       </div>
     )
--- frontend/src/components/categorias/FormCriarCategoria/index.tsx
+++ frontend/src/components/categorias/FormCriarCategoria/index.tsx
@@ -5,76 +5,104 @@ import toastAlert from "../../../utils/toastAlert";
 import { useNavigate } from "react-router-dom";
 import { useRecarregarPagina } from "../../../contexts/recarregarPagina/useRecarregarPagina";
 
-
 export interface CriarCategoria {
   nome: string;
   descricao: string;
 }
 
 interface Props {
-  setOpen: (isOpen: boolean) => void
+  setOpen: (isOpen: boolean) => void;
 }
 
-export default function FormCriarCategoria({setOpen}: Props) {
+export default function FormCriarCategoria({ setOpen }: Props) {
+  const [categoria, setCategoria] = useState<CriarCategoria>({
+    nome: "",
+    descricao: "",
+  });
+  const { recarregar, setRecarregar } = useRecarregarPagina();
+  const navigate = useNavigate();
 
-  const [categoria, setCategoria] = useState<CriarCategoria>({nome:'', descricao: ''})
-  const {recarregar, setRecarregar} = useRecarregarPagina()
-  const navigate = useNavigate()
-
-  async function cadastrarCategoria(e: FormEvent<HTMLFormElement> ) {
+  async function cadastrarCategoria(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     //Todo: Criar validações de formulário.
-    
-    const resposta = await criarCategoria(categoria)
 
-    if(resposta instanceof AxiosError) {
-      toastAlert('Não foi possível criar a categoria: ' + resposta.message, 'erro')
-      if(resposta.message.includes('401')) {
-        navigate('/login')
+    const resposta = await criarCategoria(categoria);
+
+    if (resposta instanceof AxiosError) {
+      toastAlert(
+        "Não foi possível criar a categoria: " + resposta.message,
+        "erro"
+      );
+      if (resposta.message.includes("401")) {
+        navigate("/login");
       }
     }
-    toastAlert('Categoria criado com sucesso!', "sucesso")
-    setOpen(false)
-    setRecarregar(!recarregar)  
-    console.log({ email, "senha": "protected" })
+    toastAlert("Categoria criado com sucesso!", "sucesso");
+    setOpen(false);
+    setRecarregar(!recarregar);
+    console.log({ email, senha: "protected" });
   }
 
-  function atualizarEstado(e: ChangeEvent<HTMLInputElement>){
-    setCategoria({...categoria, [e.target.name]: e.target.value })
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setCategoria({ ...categoria, [e.target.name]: e.target.value });
   }
 
   return (
     <div>
-      <form 
-      onSubmit={(e) => {cadastrarCategoria(e)}}
-      className="flex flex-col items-center justify-center gap-2">
+      <form
+        onSubmit={(e) => {
+          cadastrarCategoria(e);
+        }}
+        className="flex flex-col items-center justify-center gap-2"
+      >
         <div className="flex flex-col">
-          <label className="text-white" htmlFor="">Nome:</label>
-          <input 
-          required
-          id="Nome"
-          name="nome"
-          value={categoria.nome}
-          onChange={(e) => {atualizarEstado(e)}}
-          className="rounded-md pl-1 focus:outline-none focus:ring"
-          type="text" />
+          <label className="text-white" htmlFor="">
+            Nome:
+          </label>
+          <input
+            required
+            id="Nome"
+            name="nome"
+            value={categoria.nome}
+            onChange={(e) => {
+              atualizarEstado(e);
+            }}
+            className="rounded-md pl-1 focus:outline-none focus:ring"
+            type="text"
+          />
         </div>
         <div className="flex flex-col">
-        <label className="text-white" htmlFor="">Descricao:</label>
-          <input 
-          required
-          id="Descricao"
-          name="descricao"
-          value={categoria.descricao}
-          onChange={(e) => {atualizarEstado(e)}}
-          className="rounded-md pl-1 focus:outline-none focus:ring"
-          type="text" />
+          <label className="text-white" htmlFor="">
+            Descricao:
+          </label>
+          <input
+            required
+            id="Descricao"
+            name="descricao"
+            value={categoria.descricao}
+            onChange={(e) => {
+              atualizarEstado(e);
+            }}
+            className="rounded-md pl-1 focus:outline-none focus:ring"
+            type="text"
+          />
         </div>
-        <button 
-        type="submit"
-        className="px-10 py-2 my-2 text-white bg-green-500 border rounded-md">Criar</button>
+        <div className="flex gap-4 ">
+          <button
+            type="submit"
+            className="px-10 py-2 my-2 text-white bg-green-500 border rounded-md"
+          >
+            Criar
+          </button>
+          <button
+            onClick={() => setOpen(false)}
+            className="px-10 py-2 my-2 text-white bg-red-600 border rounded-md"
+          >
+            Sair
+          </button>
+        </div>
       </form>
     </div>
-  )
+  );
 }
