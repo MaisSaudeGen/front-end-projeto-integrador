5a1abcb8357f812e9d2156da488902183c30aa98
Elias Moura <elias.smourayt@gmail.com>
2023-11-20 08:54:11 -0300

    corrigindo tamanho da foto

--- frontend/src/pages/not-found/NotFound.tsx
+++ frontend/src/pages/not-found/NotFound.tsx
@@ -5,7 +5,8 @@ export default function NotFound() {
     <main className="flex flex-col justify-center items-center grow bg-cyan-800">
       <h1 className="text-4xl mb-4 font-bold text-white  text-center">Ops! Página não encontrada.</h1>
       <p className="mb-4 text-white text-xl">A página que você tentou acessar não existe.</p>
-      <img className="max-w-[50%] rounded-md" src={notFoundImg}></img>
+      <img className="w-[50%] 2xl:w-[30%] rounded-md" src={notFoundImg}></img>
     </main>
   );
 }
+//max-w-[25%
