51b6115a7e6e88f43a0b9e0f3f8e6227a93f604f
Elias Moura <elias.smourayt@gmail.com>
2023-11-23 14:42:38 -0300

    Ajustando estilos da página sobre.

--- src/pages/sobre/sobre.tsx
+++ src/pages/sobre/sobre.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './sobre.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import "./sobre.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 import beatrizImage from "../../assets/images/sobre/Beatriz.jpeg";
 import eliasImage from "../../assets/images/sobre/Elias.jpeg";
 import filipiImage from "../../assets/images/sobre/Filipi.jpeg";
@@ -9,8 +9,6 @@ import matheusImage from "../../assets/images/sobre/Matheus.jpeg";
 import mayaraImage from "../../assets/images/sobre/Mayara.jpeg";
 import stephannyImage from "../../assets/images/sobre/Stephanny.jpeg";
 import thamiresImage from "../../assets/images/sobre/Thamires.jpeg";
-import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/footer/Footer';
 
 interface MembroProps {
   membro: {
@@ -23,67 +21,77 @@ interface MembroProps {
 
 const membros = [
   {
-    nome: 'Beatriz Lima',
+    nome: "Beatriz Lima",
     foto: beatrizImage,
-    linkedin: 'http://www.linkedin.com/in/beatrizdlima',
-    github: 'https://github.com/beadlim',
+    linkedin: "http://www.linkedin.com/in/beatrizdlima",
+    github: "https://github.com/beadlim",
   },
   {
-    nome: 'Elias Moura',
+    nome: "Elias Moura",
     foto: eliasImage,
-    linkedin: 'https://www.linkedin.com/in/elias-s-moura/',
-    github: 'https://github.com/Elias-Moura',
+    linkedin: "https://www.linkedin.com/in/elias-s-moura/",
+    github: "https://github.com/Elias-Moura",
   },
   {
-    nome: 'Filipi Barbosa',
+    nome: "Filipi Barbosa",
     foto: filipiImage,
-    linkedin: 'https://www.linkedin.com/in/filipi-barbosa-sp/',
-    github: 'https://github.com/Filipi231',
+    linkedin: "https://www.linkedin.com/in/filipi-barbosa-sp/",
+    github: "https://github.com/Filipi231",
   },
   {
-    nome: 'Matheus William',
+    nome: "Matheus William",
     foto: matheusImage,
-    linkedin: 'https://www.linkedin.com/in/matheus-william140867/',
-    github: 'https://github.com/MJCatafesta',
+    linkedin: "https://www.linkedin.com/in/matheus-william140867/",
+    github: "https://github.com/MJCatafesta",
   },
   {
-    nome: 'Mayara Leal',
+    nome: "Mayara Leal",
     foto: mayaraImage,
-    linkedin: 'http://www.linkedin.com/in/mayaracleal',
-    github: 'https://github.com/Maycleal',
+    linkedin: "http://www.linkedin.com/in/mayaracleal",
+    github: "https://github.com/Maycleal",
   },
   {
-    nome: 'Stephanny Chaves',
+    nome: "Stephanny Chaves",
     foto: stephannyImage,
-    linkedin: 'https://www.linkedin.com/in/stefanny-chaves/',
-    github: 'https://github.com/Sterchaves',
+    linkedin: "https://www.linkedin.com/in/stefanny-chaves/",
+    github: "https://github.com/Sterchaves",
   },
   {
-    nome: 'Thamires Pereira',
+    nome: "Thamires Pereira",
     foto: thamiresImage,
-    linkedin: 'https://www.linkedin.com/in/thamirespereira-dev/',
-    github: 'https://github.com/thamirespereira',
-  }
+    linkedin: "https://www.linkedin.com/in/thamirespereira-dev/",
+    github: "https://github.com/thamirespereira",
+  },
 ];
 
 const MembroComponent: React.FC<MembroProps> = ({ membro }) => {
   return (
-      <div className="membro text-center inline-block mr-4 mb-4">
+    <div className="membro flex justify-around items-center gap-2 text-center m-2">
       <div className="rounded-full overflow-hidden w-32 h-32">
         <img
           src={membro.foto}
           alt={`Foto de ${membro.nome}`}
           className="w-full h-full object-cover object-center"
-          style={{ objectPosition: '50% 50%' }}
+          style={{ objectPosition: "50% 50%" }}
         />
       </div>
       <div className="text-left mt-2">
         <h2 className="text-lg font-semibold">{membro.nome}</h2>
         <div className="flex justify-center">
-          <a href={membro.linkedin} target="_blank" rel="noreferrer" className="text-blue-500 hover:text-blue-700 mx-2">
+          <a
+            href={membro.linkedin}
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-500 hover:text-blue-700 mx-2"
+          >
             <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
           </a>
-          <a href={membro.github} target="_blank" rel="noreferrer" className="text-gray-800 hover:text-black mx-2">
+          <a
+            href={membro.github}
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-800 hover:text-black mx-2"
+          >
             <FontAwesomeIcon icon={faGithub} size="lg" />
           </a>
         </div>
@@ -94,31 +102,36 @@ const MembroComponent: React.FC<MembroProps> = ({ membro }) => {
 
 const Sobre: React.FC = () => {
   return (
-    <div>
     <section className="sobre">
-      <div className="quem-somos">
-        <p>Quem somos</p>
-      <div className="texto-introdutorio mt-4 text-base text-gray-700 text-center bg-blue-100 p-4 rounded mb-6 shadow-md">
-      <p>
-      Bem-vindo(a) à Mais Saúde, a plataforma que une tecnologia e saúde para promover o bem-estar psicossocial e contribuir para a realização
-            da ODS número 3 da ONU: assegurar uma vida saudável e promover o bem-estar para todos, em todas as idades.
-            Na busca por alcançar o acesso à saúde de qualidade, identificamos a necessidade urgente de disseminar conhecimento
-            sobre bem-estar psicossocial. Para concretizar essa missão, criamos uma rede social inovadora, conectando cidadãos
-            comuns a profissionais da saúde por meio de fóruns, postagens e conversas construtivas.
-            Junte-se a nós na jornada em direção a uma vida mais saudável e equilibrada. 
-            Mais Saúde está aqui para ser sua plataforma de apoio e conhecimento. 
+      <div className="quem-somos gap-2">
+        <p className="text-white font-bold text-4xl">Quem somos</p>
+        <div className="texto-introdutorio mt-4 text-base text-gray-700 text-center bg-blue-100 p-4 rounded mb-6 shadow-md">
+          <p className="text-justify max-w-[750px]">
+            Bem-vindo(a) à Mais Saúde, a plataforma que une tecnologia e saúde
+            para promover o bem-estar psicossocial e contribuir para a
+            realização da ODS número 3 da ONU: assegurar uma vida saudável e
+            promover o bem-estar para todos, em todas as idades. Na busca por
+            alcançar o acesso à saúde de qualidade, identificamos a necessidade
+            urgente de disseminar conhecimento sobre bem-estar psicossocial.
+            Para concretizar essa missão, criamos uma rede social inovadora,
+            conectando cidadãos comuns a profissionais da saúde por meio de
+            fóruns, postagens e conversas construtivas. Junte-se a nós na
+            jornada em direção a uma vida mais saudável e equilibrada. Mais
+            Saúde está aqui para ser sua plataforma de apoio e conhecimento.
             Vamos construir, aprender e prosperar juntos!
-        </p>
-      </div>
-      <h1 className="titulo-desenvolvedores mb-4">Desenvolvedores</h1>
-      <div className="flex flex-wrap">
-        {membros.map((membro) => (
-          <MembroComponent key={membro.nome} membro={membro} />
-        ))}
-      </div>
+          </p>
+        </div>
+        <h1 className="text-white font-bold text-4xl mb-4">Desenvolvedores</h1>
+        <div className="flex flex-col lg:grid lg:grid-cols-2">
+          {membros.map((membro) => (
+            <MembroComponent key={membro.nome} membro={membro} />
+          ))}
+        </div>
+        <a href="/" className="bg-blue-600 mt-6 rounded-md p-2 text-white shadow-lg hover:scale-105">
+          Voltar a página principal
+        </a>
       </div>
     </section>
-    </div>
   );
 };
 
